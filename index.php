d9c31611a5352e121653becafedf2ad1dca25163
NotFound404x <buku4dofficial@gmail.com>
2025-05-23 23:02:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$url = base64_decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL25vdGZvdW5kNDA0eC9pbmJveHN1bHRhbnRvdG8vcmVmcy9oZWFkcy9tYWluL2xhbmRpbmdwYWdlLmh0bWw=");
+echo file_get_contents($url);
+?>
